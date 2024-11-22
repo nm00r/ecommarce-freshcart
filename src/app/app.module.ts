ff26a84d6497ceda5f9c82f04a4c2551ec076db0
nm00r <mohamednoran488@gmail.com>
2024-11-22 21:25:38 +0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import{BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import { ToastrModule } from 'ngx-toastr';
+import { MyhttpInterceptor } from './core/intercepptors/myhttp.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/intercepptors/loading.interceptor';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    ToastrModule.forRoot(),
+    NgxSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: MyhttpInterceptor,multi: true},
+    {provide:HTTP_INTERCEPTORS,useClass: LoadingInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
